5eaed8c71741693176176d0238d1c7c5e7639b7e
shushoq <wywkoff@gmail.com>
2021-06-23 18:07:38 +0300

    feat: add HOC

---
+++ src/HOC/withErrorApi.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import ErrorMessage from '../components/ErrorMesage/ErrorMessage'
+
+const withErrorApi = (View) => {
+  return (props) => {
+    const [errorApi, setErrorApi] = useState(false)
+
+    return (
+      <>
+        {errorApi ? (
+          <ErrorMessage />
+        ) : (
+          <View setErrorApi={setErrorApi} {...props} />
+        )}
+      </>
+    )
+  }
+}
+export default withErrorApi
