fd4dc1d87d1e93d402cf3060ac0a55828d475317
shushoq <wywkoff@gmail.com>
2021-06-23 18:07:26 +0300

    feat: add error message component

---
+++ src/components/ErrorMesage/ErrorMessage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './ErrorMessage.module.scss'
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <p className={styles.text}>
+        The dark side of the force has won. <br />
+        We cannot display data
+        <br />
+        Come back whe we fix everything.
+      </p>
+    </>
+  )
+}
+
+export default ErrorMessage
