d620af04d4fde6933b86f4213566c5196cf07e72
shushoq <wywkoff@gmail.com>
2021-06-23 18:08:12 +0300

    feat: add work with API

---
+++ src/services/getPeopleData.js
@@ -0,0 +1,16 @@
+import {
+  HTTPS,
+  SWAPI_ROOT,
+  SWAPI_PEOPLE,
+  GUIDE_IMG_EXTENSION,
+  URL_IMG_PERSON,
+} from '../constants/api'
+
+const getId = (url, category) => {
+  return url.replace(HTTPS + SWAPI_ROOT + category, '').replace(/\//g, '')
+}
+
+export const getPeopleId = (url) => getId(url, SWAPI_PEOPLE)
+
+export const getPeopleImage = (id) =>
+  `${URL_IMG_PERSON}${id}${GUIDE_IMG_EXTENSION}`
