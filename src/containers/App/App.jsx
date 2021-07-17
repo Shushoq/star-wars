c15a3b381824868c09bae2f538fab28115cb7749
Evgeniy Shushkov <59647498+Shushoq@users.noreply.github.com>
2021-07-17 11:40:41 +0300

    fix: update routing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import REPO_NAME from '@constants/repo'
+import { REPO_NAME } from '@constants/repo'
 import Header from '@components/Header'
 import routesConfig from '@routes/routesConfig'
 
