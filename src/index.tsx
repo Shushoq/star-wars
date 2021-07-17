b59428ac6b98dacf9c1fd6cac55b8ded2513687f
Evgeniy Shushkov <59647498+Shushoq@users.noreply.github.com>
2021-07-17 13:16:32 +0300

    change on ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import App from './containers/App'
-import store from '@store/store'
-import ThemeProvider from '@context/ThemeProvider'
+import store from './store/store'
+import ThemeProvider from './context/ThemeProvider'
 
 import './styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </Provider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
