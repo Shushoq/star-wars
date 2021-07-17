49f599be3434884a5b628f1214086d64258f09ab
Evgeniy Shushkov <59647498+Shushoq@users.noreply.github.com>
2021-07-17 11:39:31 +0300

    fix: change routing

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import REPO_NAME from '@constants/repo'
 import Header from '@components/Header'
 import routesConfig from '@routes/routesConfig'
 
@@ -8,15 +9,22 @@ import styles from './App.module.scss'
 
 const App = () => {
   return (
-    <div className={styles.wrapper}>
-      <Header />
+    <BrowserRouter basename={`/${REPO_NAME}/`}>
+      <div className={styles.wrapper}>
+        <Header />
 
-      <Switch>
-        {routesConfig.map(({ path, exact, component }, index) => (
-          <Route key={index} path={path} exact={exact} component={component} />
-        ))}
-      </Switch>
-    </div>
+        <Switch>
+          {routesConfig.map(({ path, exact, component }, index) => (
+            <Route
+              key={index}
+              path={path}
+              exact={exact}
+              component={component}
+            />
+          ))}
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 import App from './containers/App'
 import store from '@store/store'
@@ -11,13 +10,11 @@ import './styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
